eea9222af50a11545c6f36dc21bdbcc69eef2a1d
srikanto5120 <shreekantaray@gmail.com>
2021-10-27 15:34:28 +0600

    added

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//connect the server
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efvjx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("online-shop");
+    const productsCollection = database.collection("products");
+    console.log("connect the server");
+
+    // get api
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const page = req.query.page;
+      const size = parseInt(req.query.size);
+      const count = await cursor.count();
+      let products;
+      if (page) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+
+      res.send({
+        count,
+        products,
+      });
+    });
+    // post api by keys
+    app.post("/products/key", async (req, res) => {
+      const keys = req.body;
+      const query = { key: { $in: keys } };
+      const products = await productsCollection.find(query).toArray();
+      res.json(products);
+    });
+  } finally {
+    // client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("emajhon server is runnig");
+});
+
+app.listen(port, () => {
+  console.log("Running on", port);
+});
